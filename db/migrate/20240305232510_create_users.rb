c1e36f1551e00adfac044ad5b43b73c804dbf238
Eduardo <eduardosuarez.ucv@gmail.com>
2024-03-05 19:25:32 -0400

    adding controllers and first model

---
+++ db/migrate/20240305232510_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :lastname
+      t.string :email
+      t.string :password
+      t.string :rol
+      t.string :img
+      t.boolean :blocked
+
+      t.timestamps
+    end
+  end
+end
