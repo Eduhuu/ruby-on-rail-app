b56a6a9fdd87c2abbc93bd31242e987197590b93
Eduhuu <eduardosuarez.c97@gmail.com>
2024-06-29 21:37:05 -0400

    comment redirect_to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(content:params[:content], publication_id:params[:id], user_id:Current.user.id)
 
       if @comment.save
-        redirect_to 
+        # redirect_to 
       else
         flash[:error] = "Failed to create comment: #{@comment.errors.full_messages.join(', ')}"
         render :new, status: :unprocessable_entity
